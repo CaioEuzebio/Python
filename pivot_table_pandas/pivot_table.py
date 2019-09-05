788058e01275c7d1c9062a350e1de6cbc51eb75a
Caio Euzébio <39926679+CaioEuzebio@users.noreply.github.com>
2019-09-05 06:54:19 -0300

    Update pivot_table.py

--- pivot_table_pandas/pivot_table.py
+++ pivot_table_pandas/pivot_table.py
@@ -4,5 +4,5 @@ df = pd.read_csv("output.csv", low_memory=False)
 
 #Creating a new dataframe as pivot table in this case with aggfunc as (sum)
 
-pivotpar = df.pivot_table(index='PART NUMBER', aggfunc='sum')
+pivotpar = df.pivot_table(index='Row Labels Column Name', aggfunc='sum')
 
